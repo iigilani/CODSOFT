d7ee977f05873f0fa43857b4f7fd93b3c8601672
iigilani <iigilani@gmail.com>
2024-06-12 15:15:04 +0500

    INDEX+STY+JS

---
+++ CODSOFT.js
@@ -0,0 +1,19 @@
+const   slides = document.querySelectorAll(".slide")
+var counter = 0;
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName(".dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
