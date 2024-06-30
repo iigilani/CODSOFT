9de786f82a045f91c5b20b5fd6ac4540d4332a21
iigilani <iigilani@gmail.com>
2024-06-30 16:03:00 +0500

    STY+JS

--- CODSOFT.js
+++ CODSOFT.js
@@ -89,7 +89,7 @@ function myFunction() {
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropBtn')) {
       var dropdowns = document.getElementsByClassName(".dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
